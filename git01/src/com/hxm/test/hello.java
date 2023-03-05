4de6f3d44769c6b5e081c7bcb7ffa13559598624
hxm <1914254966@qq.com>
2023-03-05 16:08:52 +0800

    2.0

--- git01/src/com/hxm/test/hello.java
+++ git01/src/com/hxm/test/hello.java
@@ -3,5 +3,6 @@ package com.hxm.test;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("2.0 hello git");
     }
 }
